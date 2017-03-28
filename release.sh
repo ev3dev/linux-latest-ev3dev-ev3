3b713f392bd0136addcc1f4768fdd745819777ac
David Lechner <david@lechnology.com>
2017-03-28 16:30:31 -0500

    Target stretch

--- release.sh
+++ release.sh
@@ -6,9 +6,11 @@ set -e
 
 source=$(dpkg-parsechangelog -S Source)
 version=$(dpkg-parsechangelog -S Version)
+distribution=$(dpkg-parsechangelog -S Distribution)
+codename=$(debian-distro-info --codename --${distribution})
 
-OS=debian DIST=jessie ARCH=armel pbuilder-ev3dev build
-debsign ~/pbuilder-ev3dev/debian/jessie-armel/${source}_${version}_armel.changes
-dput ev3dev-debian ~/pbuilder-ev3dev/debian/jessie-armel/${source}_${version}_armel.changes
+OS=debian DIST=${codename} ARCH=armel pbuilder-ev3dev build
+debsign ~/pbuilder-ev3dev/debian/${codename}-armel/${source}_${version}_armel.changes
+dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-armel/${source}_${version}_armel.changes
 
 gbp buildpackage --git-tag-only
